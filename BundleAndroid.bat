2ba42c3fb4eb95931b3d4e41689d85efa61d4d86
deepakmega <deepak.mega@gmail.com>
2015-12-21 13:15:29 +0530

    Updated react-native version to 17

--- BundleAndroid.bat
+++ BundleAndroid.bat
@@ -1,3 +1,3 @@
-curl "http://localhost:8081/index.android.bundle?platform=android" -dev=false -o "android/app/src/main/assets/index.android.bundle"
+REM curl "http://localhost:8081/index.android.bundle?platform=android" -dev=false -o "android/app/src/main/assets/index.android.bundle"
 
-REM react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
+react-native bundle --platform android --dev false --entry-file index.android.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
